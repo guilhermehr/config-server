d3b0244a3a117342b82a59888ecb4c0a66368aec
Guilherme Henrique Ribeiro <guilherme.h.ribeiro@bradesco.com.br>
2022-03-21 23:57:29 -0300

    inicio

---
+++ src/test/java/br/com/alura/microservice/config/ConfigServerApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.alura.microservice.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootTest
+@EnableConfigServer
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
